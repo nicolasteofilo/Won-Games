9597e567e5012b285444dc1232b4c93d50ffccd2
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-18 21:39:55 -0300

    style: configure theme provider styled componens

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import { GlobalStyles } from 'styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>Next.js Template</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
@@ -18,6 +20,6 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
