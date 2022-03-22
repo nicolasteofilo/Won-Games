1daf3c8c77588f7a72318992e541d7a4484d7884
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-21 22:42:16 -0300

    feat: Add Button with diferents sizes

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -3,7 +3,14 @@ import { Button } from '.'
 
 export default {
   title: 'Button',
-  component: Button
+  component: Button,
+  argTypes: {
+    children: { type: 'string' }
+  }
 } as Meta
 
-export const Basic: Story = () => <Button />
+export const Basic: Story = (args) => <Button {...args} />
+
+Basic.args = {
+  children: 'Hello Button'
+}
