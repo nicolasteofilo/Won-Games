9ca0732d8a201256f25fd60e5a9f367b604b45e3
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-21 22:55:24 -0300

    feat: Add Button tests

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,10 +1,36 @@
-import { screen, render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
 import { Button } from '.'
+import 'jest-styled-components'
+import theme from '../../styles/theme'
 
 describe('<Button />', () => {
-  it('should render correctly', () => {
-    const Button = render(<Button />)
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Won Games</Button>)
 
-    expect(Button).toBeInTheDocument()
-  }),
+    expect(screen.getByRole('button', { name: /Won Games/i })).toHaveStyle({
+      fontSize: '1.4rem',
+      height: '4rem',
+      padding: `${theme.spacings.xxsmall} ${theme.spacings.medium}`
+    })
+  })
+
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">Won Games</Button>)
+
+    expect(screen.getByRole('button', { name: /Won Games/i })).toHaveStyle({
+      height: '3rem',
+      fontSize: `${theme.font.sizes.xsmall}`
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">Won Games</Button>)
+
+    expect(screen.getByRole('button', { name: /Won Games/i })).toHaveStyle({
+      height: '5rem',
+      fontSize: `${theme.font.sizes.medium}`,
+      padding: `${theme.spacings.xxsmall} ${theme.spacings.xlarge}`
+    })
+  })
 })
