a3d6f045b5249229aca1107daa98cdb63d2ff2d2
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-23 21:44:16 -0300

    feat: Add logic open/close to menu

--- src/components/Menu/Menu.spec.tsx
+++ src/components/Menu/Menu.spec.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from '../../utils/tests/helpers'
 import { Menu } from '.'
 
@@ -10,5 +10,27 @@ describe('<Menu />', () => {
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
     expect(screen.getByLabelText(/Search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/Open Shopping Cart/i)).toBeInTheDocument()
+  })
+  it('should render the open/close menu', () => {
+    renderWithTheme(<Menu />)
+
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({
+      opacity: 0
+    })
+
+    fireEvent.click(screen.getByLabelText(/Open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({
+      opacity: 1
+    })
+
+    fireEvent.click(screen.getByLabelText(/Close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({
+      opacity: 0
+    })
   })
 })
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,29 +1,39 @@
 import { MenuAlt1 as MenuIcon } from '@styled-icons/heroicons-outline'
 import {
   Search as SearchIcon,
-  AddShoppingCart as ShoppingCartIcon
+  AddShoppingCart as ShoppingCartIcon,
+  Close as CloseIcon
 } from 'styled-icons/material-outlined'
 
 import { Logo } from 'components/Logo'
 import * as S from './styles'
+import { useState } from 'react'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open menu" />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile aria-label="Logo" />
-    </S.LogoWrapper>
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open menu" />
       </S.IconWrapper>
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open Shopping Cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile aria-label="Logo" />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export { Menu }
