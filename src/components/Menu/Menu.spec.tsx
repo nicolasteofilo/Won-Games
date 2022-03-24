470062f92dc1e1c81f69a775d894f2e6d1c659d7
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-23 21:25:03 -0300

    feat: Add Menu first tests and accessibility

--- src/components/Menu/Menu.spec.tsx
+++ src/components/Menu/Menu.spec.tsx
@@ -1,10 +1,14 @@
-import { screen, render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
 import { Menu } from '.'
 
 describe('<Menu />', () => {
-  it('should render correctly', () => {
-    const Menu = render(<Menu />)
+  it('should render menu', () => {
+    renderWithTheme(<Menu />)
 
-    expect(Menu).toBeInTheDocument()
-  }),
+    expect(screen.getByLabelText(/Open menu/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/Search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/Open Shopping Cart/i)).toBeInTheDocument()
+  })
 })
