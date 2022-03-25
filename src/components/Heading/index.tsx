9b538d3a6e538e86d8d1470c38bc5fc9a156a923
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-24 21:50:59 -0300

    feat: add option color for line bottom/left in Heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children?: React.ReactNode
   color?: 'black' | 'white'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    size={size}
+    color={color}
+    lineColor={lineColor}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+  >
     {children}
   </S.Wrapper>
 )
