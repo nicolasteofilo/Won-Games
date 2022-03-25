1e2f6fdadfdbb2d27aa022bc8f0ce6717a362aba
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-25 20:31:43 -0300

    feat: create inital structure do Banner component

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.main`
+export const Wrapper = styled.main``
 
-`
+type ImageProps = {
+  src: string
+}
+
+export const Imgage = styled.div<ImageProps>``
+
+export const Caption = styled.div``
+
+export const Title = styled.h2``
+
+export const Subtitle = styled.h2``
