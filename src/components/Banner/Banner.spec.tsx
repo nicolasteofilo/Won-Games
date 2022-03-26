cefef82c5feda2117d54355de4e275d9c28b5808
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-26 11:56:03 -0300

    test: add test for Banner

--- src/components/Banner/Banner.spec.tsx
+++ src/components/Banner/Banner.spec.tsx
@@ -1,10 +1,31 @@
 import { screen, render } from '@testing-library/react'
 import { Banner } from '.'
+import { renderWithTheme } from '../../utils/tests/helpers'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
 
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    const Banner = render(<Banner />)
+    renderWithTheme(<Banner {...props} />)
 
-    expect(Banner).toBeInTheDocument()
-  }),
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', {
+        name: /Play the new CrashLands season/i
+      })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('img', {
+        name: /Defy death/i
+      })
+    ).toBeInTheDocument()
+  })
 })
