feb5d447917a1a34c7e71061be4ae6ceb1424812
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-02 11:15:27 -0300

    style: create styles for Container component

---
+++ src/components/Container/Container.spec.tsx
@@ -0,0 +1,36 @@
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { Container } from '.'
+
+describe('<Container />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <p>Won Games</p>
+      </Container>
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container
+    )
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div
+        class="c0"
+      >
+        <p>
+          Won Games
+        </p>
+      </div>
+    `)
+  })
+})
