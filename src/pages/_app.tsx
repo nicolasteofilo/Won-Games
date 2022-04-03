e7852df86eacf8d041ae861afd5b22c39180d180
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-03 16:14:40 -0300

    refactor: remove <Main /> component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,10 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
-        <meta
-          name="description"
-          content="Next.js template with typescript, jest, styled-components"
-        />
+        <meta name="description" content="The best Games Store" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
