ba9382bda35399e469ff5405d435bbdc2ce8d173
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-04 17:04:27 -0300

    test: add styles in Home

--- src/templates/Home/Home.spec.tsx
+++ src/templates/Home/Home.spec.tsx
@@ -1,15 +1,57 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+import '../../../.jest/match-media-mock'
+
 import { Home } from '.'
+
+const props = {
+  banners: bannerMock,
+  newGames: gamesMock,
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: gamesMock,
+  upcommingGames: gamesMock,
+  upcommingHighligth: highlightMock,
+  upcommingMoreGames: gamesMock,
+  freeGames: gamesMock,
+  freeHighligth: highlightMock
+}
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/Open menu/i)).toBeInTheDocument()
     expect(screen.getByText(/Contact Us/i)).toBeInTheDocument()
     expect(screen.getByText(/Follow us/i)).toBeInTheDocument()
     expect(screen.getByText(/Links/i)).toBeInTheDocument()
     expect(screen.getByText(/Location/i)).toBeInTheDocument()
+  })
+
+  it('should render sections', () => {
+    renderWithTheme(<Home {...props} />)
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render section elements', () => {
+    renderWithTheme(<Home {...props} />)
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections for 4 cards each = 5x4 = 20)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+    // highlight
+    expect(screen.getAllByText(/Biomutant/i)).toHaveLength(3)
   })
 })
