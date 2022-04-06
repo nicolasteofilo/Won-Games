979d6e775d477a94901e39af65e8fc55d2e53210
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-06 14:06:19 -0300

    feat: add onCheck method

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,22 +1,43 @@
+import { InputHTMLAttributes, useState } from 'react'
 import * as S from './styles'
 
 export type CheckboxProps = {
   label?: string
   labelFor?: string
   labelColor?: 'black' | 'white'
-}
+  onCheck?: (status: boolean) => void
+} & InputHTMLAttributes<HTMLInputElement>
 
 export const Checkbox = ({
   label,
   labelFor = '',
-  labelColor = 'white'
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <S.Input id={labelFor} type="checkbox" />
-    {!!label && (
-      <S.Label labelColor={labelColor} htmlFor={labelFor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+  labelColor = 'white',
+  onCheck
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(false)
+
+  const onChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    if (onCheck) {
+      onCheck(status)
+    }
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+      />
+      {!!label && (
+        <S.Label labelColor={labelColor} htmlFor={labelFor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
