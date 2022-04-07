70f80d4f4527ce8e0c2b4380250c63a62f44f925
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-07 17:25:06 -0300

    test: create initial structure for Auth template

--- src/templates/Auth/Auth.spec.tsx
+++ src/templates/Auth/Auth.spec.tsx
@@ -1,10 +1,18 @@
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 import { Auth } from '.'
 
 describe('<Auth />', () => {
-  it('should render correctly', () => {
-    const { container } = render(<Auth />)
+  it('should render correctly logos, title and children', () => {
+    renderWithTheme(<Auth title="Sing up">children</Auth>)
 
-    expect(container.firstChild).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+    screen.logTestingPlaygroundURL()
+
+    expect(
+      screen.getByRole('heading', { name: /sing up/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(/children/i)).toBeInTheDocument()
   })
 })
