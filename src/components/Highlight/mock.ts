5fc44aca92813800cf55bb3481ce3ae914830f56
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-13 15:34:47 -0300

    test: add Showcase component

--- src/components/Highlight/mock.ts
+++ src/components/Highlight/mock.ts
@@ -1,7 +1,7 @@
 export default {
   title: 'Biomutant',
   subtitle:
-    'Preorder now to get the Special Mercenary Class DLC and the Official Soundtrack.    ',
+    'Preorder now to get the Special Mercenary Class DLC and the Official Soundtrack.',
   buttonLabel: 'Pre order now',
   buttonLink: '#',
   backgroundImage: '/img/red-dead-img.jpg'
--- src/components/Showcase/Showcase.spec.tsx
+++ src/components/Showcase/Showcase.spec.tsx
@@ -1,10 +1,69 @@
-import { render } from '@testing-library/react'
+import '../../../.jest/match-media-mock'
+
+import { screen } from '@testing-library/react'
 import { Showcase } from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import highlightMock from 'components/Highlight/mock'
+import gameCardSliderMock from 'components/GameCardSlider/mock'
 
 describe('<Showcase />', () => {
   it('should render correctly', () => {
-    const { container } = render(<Showcase />)
+    renderWithTheme(
+      <Showcase
+        title="Most Popular"
+        highlight={highlightMock}
+        games={gameCardSliderMock}
+      />
+    )
 
-    expect(container.firstChild).toBeInTheDocument()
+    // Title
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+
+    // Highlight
+    expect(screen.getByText(highlightMock.title)).toBeInTheDocument()
+
+    // GameCardSlider
+    expect(screen.queryAllByText(gameCardSliderMock[0].title)).toHaveLength(4)
+  })
+
+  it('should render correctly with no title', () => {
+    renderWithTheme(
+      <Showcase highlight={highlightMock} games={gameCardSliderMock} />
+    )
+
+    // Highlight
+    expect(screen.getByText(highlightMock.title)).toBeInTheDocument()
+
+    // GameCardSlider
+    expect(screen.queryAllByText(gameCardSliderMock[0].title)).toHaveLength(4)
+  })
+
+  it('should render correctly with no highlight', () => {
+    renderWithTheme(
+      <Showcase title="Most Popular" games={gameCardSliderMock} />
+    )
+
+    // Title
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+
+    // GameCardSlider
+    expect(screen.queryAllByText(gameCardSliderMock[0].title)).toHaveLength(4)
+  })
+
+  it('should render correctly with no games', () => {
+    renderWithTheme(<Showcase title="Most Popular" highlight={highlightMock} />)
+
+    // Title
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+
+    // Highlight
+    expect(screen.getByText(highlightMock.title)).toBeInTheDocument()
   })
 })
