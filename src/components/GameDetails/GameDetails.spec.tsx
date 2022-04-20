b6a5a959020d96d5943687624a927f3e2cc8f9c8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-04-20 14:47:30 -0300

    test: formatted date with Intl in GameDetails

--- src/components/GameDetails/GameDetails.spec.tsx
+++ src/components/GameDetails/GameDetails.spec.tsx
@@ -1,10 +1,44 @@
-import { render } from '@testing-library/react'
-import { GameDetails } from '.'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { GameDetails, GameDetailsProps } from '.'
+
+const props: GameDetailsProps = {
+  developer: 'GearBox Software',
+  platforms: ['linux', 'mac', 'windows'],
+  releaseDate: '2019-11-16T23:00:00'
+}
 
 describe('<GameDetails />', () => {
-  it('should render correctly', () => {
-    const { container } = render(<GameDetails />)
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
 
-    expect(container.firstChild).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Developer/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Release Date/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Platforms/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Publishers/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Rating/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Genres/i })).toBeInTheDocument()
+  })
+
+  it('should render the platform icons', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByTitle('linux')).toBeInTheDocument()
+    expect(screen.getByTitle('mac')).toBeInTheDocument()
+    expect(screen.getByTitle('windows')).toBeInTheDocument()
+  })
+
+  it('should be render the formatted date', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/Nov 16, 2019/i)).toBeInTheDocument()
   })
 })
