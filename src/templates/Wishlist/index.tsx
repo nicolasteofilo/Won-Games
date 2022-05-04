e9e2b5a64a34fe64093dbf7d431c7fad7d45f106
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-04 18:54:25 -0300

    feat: add showcase in wishlist page

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -2,13 +2,31 @@ import { Base } from 'templates/Base'
 import { Container } from 'components/Container'
 import { Heading } from 'components/Heading'
 
-const Wishlist = () => (
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+
+import { Showcase } from 'components/Showcase'
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendHighlight,
+  recommendedGames
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendHighlight}
+    />
   </Base>
 )
 
