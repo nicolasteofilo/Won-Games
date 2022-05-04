ecadfcdb92573e8f0e05172d88c809a99660ead8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-04 19:01:44 -0300

    test: ensure render correctly

--- src/templates/Wishlist/Wishlist.spec.tsx
+++ src/templates/Wishlist/Wishlist.spec.tsx
@@ -1,10 +1,34 @@
-import { render } from '@testing-library/react'
-import { Wishlist } from '.'
+import '../../../.jest/match-media-mock'
+import { screen } from '@testing-library/react'
+import { Wishlist, WishlistTemplateProps } from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props: WishlistTemplateProps = {
+  recommendedGames: gamesMock.slice(0, 5),
+  recommendHighlight: highlightMock
+}
+
+jest.mock('components/Showcase', () => {
+  return {
+    __esModule: true,
+    Showcase: function Mock() {
+      return <div data-testid="mock showcase"></div>
+    }
+  }
+})
 
 describe('<Wishlist />', () => {
   it('should render correctly', () => {
-    const { container } = render(<Wishlist />)
+    renderWithTheme(<Wishlist {...props} />)
 
-    expect(container.firstChild).toBeInTheDocument()
+    expect(screen.getByTestId(/mock showcase/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', {
+        name: /Wishlist/i
+      })
+    )
   })
 })
