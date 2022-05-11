7acdae5e586a2a7c857bc02e9e0c29e0ebe1f2a7
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-11 16:40:43 -0300

    test: create CartList

---
+++ src/components/CartList/CartList.spec.tsx
@@ -0,0 +1,20 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { CartList } from '.'
+
+import cartListMock from './mock'
+
+describe('<CartList />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(
+      <CartList items={cartListMock} total="R$ 330.00" />
+    )
+
+    expect(screen.getAllByRole('heading')).toHaveLength(2)
+    expect(
+      screen.getByRole('img', { name: /Red Dead Redemption 2/i })
+    ).toHaveAttribute('src', cartListMock[0].img)
+    expect(screen.getByText('R$ 330.00')).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
