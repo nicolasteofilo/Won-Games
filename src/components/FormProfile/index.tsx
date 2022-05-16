59408c2a01b41655c0965b59f7132bdab5ed04b3
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-16 15:21:24 -0300

    feat: ensure FormProfile initial strcture

---
+++ src/components/FormProfile/index.tsx
@@ -0,0 +1,46 @@
+import { Heading } from 'components/Heading'
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+
+import * as S from './styles'
+
+const FormProfile = () => (
+  <S.Wrapper>
+    <Heading lineBottom color="black" size="small">
+      My profile
+    </Heading>
+    <S.Form>
+      <TextField
+        name="name"
+        placeholder="Name"
+        label="Name"
+        initialValue="John Doe"
+      />
+      <TextField
+        name="email"
+        type="email"
+        placeholder="E-mail"
+        initialValue="joedoe@email.com"
+        label="E-mail"
+        disabled
+      />
+      <TextField
+        name="password"
+        type="password"
+        placeholder="type your password"
+        label="Password"
+      />
+      <TextField
+        name="new_password"
+        type="new_password"
+        placeholder="new password"
+        label="New password"
+      />
+      <Button type="submit" size="large">
+        Save
+      </Button>
+    </S.Form>
+  </S.Wrapper>
+)
+
+export { FormProfile }
