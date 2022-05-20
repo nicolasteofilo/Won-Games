bd67f00a54597802244b1ce08c7b1923bc36f600
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-19 21:04:26 -0300

    test: ensure OrdersList render the games items

---
+++ src/components/OrdersList/OrdersList.spec.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { OrdersList } from '.'
+
+import mock from './mock'
+
+jest.mock('components/Empty', () => {
+  return {
+    __esModule: true,
+    Empty: function Mock() {
+      return <div data-testid="mock Empty" />
+    }
+  }
+})
+
+jest.mock('components/GameItem', () => {
+  return {
+    __esModule: true,
+    GameItem: function Mock({ children }: { children: React.ReactNode }) {
+      return <div data-testid="mock GameItem">{children}</div>
+    }
+  }
+})
+
+describe('<OrdersList />', () => {
+  it('should render the games items', () => {
+    renderWithTheme(<OrdersList items={mock} />)
+
+    expect(screen.getByRole('heading', { name: /My orders/i }))
+    expect(screen.getAllByTestId('mock GameItem')).toHaveLength(2)
+  })
+})
