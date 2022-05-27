d283b02b31ba724811451d24ecbc09f16dc3eb96
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-26 21:39:06 -0300

    test: ensure ExploreSidebar filter if altern between radio options

--- src/components/ExploreSidebar/ExploreSidebar.spec.tsx
+++ src/components/ExploreSidebar/ExploreSidebar.spec.tsx
@@ -84,5 +84,20 @@ describe('<ExploreSidebar />', () => {
       })
     )
     expect(onFilter).toBeCalledWith({ windows: true, sort_by: 'low-to-high' })
+  }) 
+
+  it('should altern between radio options', () => {
+    const onFilter = jest.fn()
+
+    renderWithTheme(<ExploreSidebar items={items} onFilter={onFilter} />)
+    fireEvent.click(screen.getByLabelText(/high to low/i))
+    fireEvent.click(screen.getByLabelText(/low to high/i))
+    fireEvent.click(
+      screen.getByRole('button', {
+        name: /filter/i
+      })
+    )
+    expect(onFilter).toBeCalledWith({ sort_by: 'low-to-high' })
   })
+
 })
