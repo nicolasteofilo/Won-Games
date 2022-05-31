a78cf08ed88fc373231e115b0a7e508c67d4dc88
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-30 22:04:33 -0300

    test: ensure Games page render

--- src/templates/Games/Games.spec.tsx
+++ src/templates/Games/Games.spec.tsx
@@ -1,10 +1,39 @@
-import { screen, render } from '@testing-library/react'
-import { Games } from '.'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { GamesTemplate } from '.'
+
+import filterItemsMock from 'components/ExploreSidebar/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+
+jest.mock('templates/Base', () => {
+  return {
+    __esModule: true,
+    Base: function Mock({ children }: { children: React.ReactNode }) {
+      return <div data-testid="mock base">{children}</div>
+    }
+  }
+})
+
+jest.mock('components/ExploreSidebar', () => {
+  return {
+    __esModule: true,
+    ExploreSidebar: function Mock() {
+      return <div data-testid="mock exploreSidebar" />
+    }
+  }
+})
 
 describe('<Games />', () => {
-  it('should render correctly', () => {
-    const { container } = render(<Games />)
+  it('should render sections', () => {
+    renderWithTheme(
+      <GamesTemplate filterItems={filterItemsMock} games={[gamesMock[0]]} />
+    )
 
-    expect(container.firstChild).toBeInTheDocument()
+    expect(screen.getByTestId('mock base')).toBeInTheDocument()
+    expect(screen.getByTestId('mock exploreSidebar')).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /show more/i })
+    ).toBeInTheDocument()
   })
 })
