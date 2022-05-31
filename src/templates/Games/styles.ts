aed28694923f59ade3bbd0ba6f950d325e37727f
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-05-30 21:59:09 -0300

    feat: compose Games Template

--- src/templates/Games/styles.ts
+++ src/templates/Games/styles.ts
@@ -1,5 +1,28 @@
-import styled from 'styled-components'
+import media from 'styled-media-query'
+import { Container } from 'components/Container'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main`
+export const Main = styled(Container)`
+  ${({ theme }) => css`
+    ${media.greaterThan(`medium`)`
+      display: grid;
+      grid-template-columns: 26rem 1fr;
+      gap: ${theme.grid.gutter};
+    `}
+  `}
+`
 
+export const ShowMore = styled.div`
+  ${({ theme }) => css`
+    text-align: center;
+    padding: ${theme.spacings.medium};
+    text-transform: uppercase;
+    font-weight: bold;
+    color: ${theme.colors.white};
+    cursor: pointer;
+
+    > svg {
+      color: ${theme.colors.primary};
+    }
+  `}
 `
