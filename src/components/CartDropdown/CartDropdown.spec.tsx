781c98fd5fd7524947adbd9134dadc0cb2be30b6
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-01 22:45:01 -0300

    test: ensure CartDropdown render <CartIcon />  and its badge

--- src/components/CartDropdown/CartDropdown.spec.tsx
+++ src/components/CartDropdown/CartDropdown.spec.tsx
@@ -1,10 +1,14 @@
-import { screen, render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 import { CartDropdown } from '.'
 
-describe('<CartDropdown />', () => {
-  it('should render correctly', () => {
-    const { container } = render(<CartDropdown />)
+import mockItems from 'components/CartList/mock'
 
-    expect(container.firstChild).toBeInTheDocument()
+describe('<CartDropdown />', () => {
+  it('should render <CartIcon />  and its badge', () => {
+    renderWithTheme(<CartDropdown items={mockItems} total="U$ 10,00" />)
+
+    expect(screen.getByLabelText(/Shopping Cart/i)).toBeInTheDocument()
+    expect(screen.getByText(`${mockItems.length}`)).toBeInTheDocument()
   })
 })
