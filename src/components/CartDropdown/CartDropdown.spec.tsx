578db5b21d6a5044b4d912319f8dcc1031741562
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-01 22:49:14 -0300

    test: ensure CartDropdown render dropdown content with cart items and total

--- src/components/CartDropdown/CartDropdown.spec.tsx
+++ src/components/CartDropdown/CartDropdown.spec.tsx
@@ -11,4 +11,11 @@ describe('<CartDropdown />', () => {
     expect(screen.getByLabelText(/Shopping Cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${mockItems.length}`)).toBeInTheDocument()
   })
+
+  it('should render dropdown content with cart items and total', () => {
+    renderWithTheme(<CartDropdown items={mockItems} total="U$ 10,00" />)
+
+    expect(screen.getByText('U$ 10,00')).toBeInTheDocument()
+    expect(screen.getByText(`${mockItems[0].title}`)).toBeInTheDocument()
+  })
 })
