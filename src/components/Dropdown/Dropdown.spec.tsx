e493e6bffe0749cbe156faaae5f9ffad5f0741bf
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-06-01 21:59:54 -0300

    test: ensure CartIcon render the open/close dropdown

--- src/components/Dropdown/Dropdown.spec.tsx
+++ src/components/Dropdown/Dropdown.spec.tsx
@@ -1,11 +1,39 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import userEvent from '@testing-library/user-event'
 import { Dropdown } from '.'
 
 describe('<Dropdown />', () => {
-  it('should render correctly', () => {
-    const { container } = renderWithTheme(<Dropdown />)
+  it('should render correctly with title and content', () => {
+    renderWithTheme(
+      // eslint-disable-next-line react/no-children-prop
+      <Dropdown title="Click to open" children="content" />
+    )
 
-    expect(container.firstChild).toBeInTheDocument()
+    expect(screen.getByText(/Click to open/i)).toBeInTheDocument()
+    expect(screen.getByText(/content/i)).toBeInTheDocument()
+  })
+
+  it('should render the open/close dropdown', async () => {
+    renderWithTheme(
+      // eslint-disable-next-line react/no-children-prop
+      <Dropdown title="Click to open">
+        <p>content</p>
+      </Dropdown>
+    )
+
+    const content = screen.getByText(/content/i).parentElement
+
+    expect(content).toHaveAttribute('aria-hidden', 'true')
+    expect(content).toHaveStyle({
+      opacity: 0
+    })
+
+    await userEvent.click(screen.getByText(/Click to open/i))
+
+    expect(content).toHaveStyle({
+      opacity: 1
+    })
+    expect(content).toHaveAttribute('aria-hidden', 'false')
   })
 })
